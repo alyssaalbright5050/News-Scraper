f759243af6c0805615beb328c0a565387ac03f76
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-02-12 20:24:51 -0600

    setup server.js

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
+//Require our dependencies
 var express = require("express");
-
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+//Setup our PORT
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+//Setup an Express Router
 var router = express.Router();
 
+//Designate our public folder as a static directory
 app.use(express.static(__dirname * "/public"));
 
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlendcoded({
+  extended: false;
+}));
+
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function(error) {
+  if(error){
+    console.log(error);
+  }
+  else{
+    console.log("mongoose connection is successful");
+  }
+});
 
 app.listen(PORT, function(){
   console.log("listening on port" + PORT);
