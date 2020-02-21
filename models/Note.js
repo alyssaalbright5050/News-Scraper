6e5634d7348301627cd580f6833672cb75039cf6
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-02-20 19:55:06 -0600

    checking note.js for typos

--- models/Note.js
+++ models/Note.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var noteSchema = new Schema ({
+
 _headlineID: {
     type: Schema.Types.ObjectId,
     ref: "Headline"
@@ -12,6 +13,6 @@ noteText: String
 
 });
 
-var Note = mongoose.model("Note", noteSChema);
+var Note = mongoose.model("Note", noteSchema);
 
 module.exports = Note;
